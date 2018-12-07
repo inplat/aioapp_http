f51aa1d4386e006b2ebcbdba13a8a3a3c1e8ecac
Konstantin Stepanov <konstantin.stepanov@inplat.ru>
2018-12-07 15:33:19 +0300

    bump version

--- aioapp_http/__init__.py
+++ aioapp_http/__init__.py
@@ -16,7 +16,7 @@ from aioapp.tracer import (Span, CLIENT, SERVER, HTTP_PATH, HTTP_METHOD,
                            HTTP_STATUS_CODE,
                            HTTP_URL, SPAN_TYPE, SPAN_KIND)
 
-__version__ = '0.0.1b2'
+__version__ = '0.0.1b4'
 
 SPAN_TYPE_HTTP = 'http'
 SPAN_KIND_HTTP_IN = 'in'
